4fa194921408ad36476f87df43c26f10f27ad961
Amaan Malik <amikm1077@gmail.com>
2026-03-05 12:27:35 +0530

    Token fetch and creation done from backend

--- DCEX/backend/src/controllers/userController.ts
+++ DCEX/backend/src/controllers/userController.ts
@@ -14,19 +14,33 @@ export const getTokens = async (req: Request, res: Response) => {
     const supportedTokens = await getSupportedTokens();
 
     const balances = await Promise.all(
-      supportedTokens.map((token) => getAccountBalance(token, address)),
+      supportedTokens.map((token) =>
+        getAccountBalance(token, address as string),
+      ),
     );
+
+    const tokens = supportedTokens.map((token, index) => ({
+      token,
+      balance: balances[index],
+      usdBalance: balances[index] * Number(token.price),
+    }));
+
+    return res.json({
+      tokens,
+      totalBalnce: tokens.reduce((acc, val) => acc + val.usdBalance, 0),
+    });
   } catch (error) {
     console.log("Error while getting tokens: ", error);
   }
 };
 
+//function for getting usser balances
 const getAccountBalance = async (
   token: { name: string; mint: string; native: boolean },
   address: string,
 ) => {
   if (token.native) {
-    let balance = await connection.getBalance(new PublicKey(address));
+    const balance = await connection.getBalance(new PublicKey(address));
     return balance / LAMPORTS_PER_SOL;
   }
 
@@ -34,8 +48,12 @@ const getAccountBalance = async (
     new PublicKey(token.mint),
     new PublicKey(address),
   );
-  const account = await getAccount(connection, ata);
-  const mint = await getMint(connection, new PublicKey(token.mint));
+  try {
+    const account = await getAccount(connection, ata);
+    const mint = await getMint(connection, new PublicKey(token.mint));
 
-  return Number(account.amount) / (10 ** mint.decimals);
+    return Number(account.amount) / 10 ** mint.decimals;
+  } catch (error) {
+    console.log("error while creating user account: ", error);
+  }
 };
--- DCEX/backend/src/lib/constants.ts
+++ DCEX/backend/src/lib/constants.ts
@@ -2,28 +2,37 @@ import { Connection } from "@solana/web3.js";
 import axios from "axios";
 
 let LAST_UPDATED: number | null = null;
-let TOKEN_REFRESH_INTERVAL = 60 * 1000;
+const TOKEN_REFRESH_INTERVAL = 60 * 1000;
 let prices: { [key: string]: { price: string } } = {};
 
-export let SUPPORTED_TOKENS: {
+export interface TokenDetails {
   name: string;
   mint: string;
   native: boolean;
-}[] = [
+  image: string;
+}
+
+export const SUPPORTED_TOKENS: TokenDetails[] = [
   {
     name: "USDC",
     mint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
     native: false,
+    image:
+      "https://imgs.search.brave.com/U7bNRac5otCvScvRngTMv3U1kN6EqXIYzLfKpJurowY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/Y3JlYXRlLnZpc3Rh/LmNvbS9hcGkvbWVk/aWEvc21hbGwvNjgx/NTM1OTQyL3N0b2Nr/LXBob3RvLXVzZGMt/dmlydHVhbC1jdXJy/ZW5jeS1pbWFnZXMt/ZGlnaXRhbC1iYWNr/Z3JvdW5kLWlsbHVz/dHJhdGlvbnM",
   },
   {
     name: "USDT",
     mint: "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
     native: false,
+    image:
+      "https://imgs.search.brave.com/BAwveJzQFcC6cmP4ZJ4Wrlbfk5Yxh_uKPBZ1IWE066Y/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4z/ZC5pY29uc2NvdXQu/Y29tLzNkL3ByZW1p/dW0vdGh1bWIvdXNk/dC1jb2luLTNkLWlj/b24tcG5nLWRvd25s/b2FkLTQxNjAwMTku/cG5n",
   },
   {
     name: "SOL",
     mint: "So11111111111111111111111111111111111111112",
-    native: false,
+    native: true,
+    image:
+      "https://imgs.search.brave.com/WYTZ82WAzCLOV0hNFdR8O3FtMcdUZtlMXB61Ev-Oi9g/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1wc2Qv/c29sYW5hLXNvbC1j/cnlwdG9jdXJyZW5j/eS1jb2luLTNkLXJl/bmRlcmluZy1pc29s/YXRlZF8xNTU1ODIt/ODAzMy5qcGc_c2Vt/dD1haXNfaHlicmlk/Jnc9NzQwJnE9ODA",
   },
 ];
 
@@ -43,6 +52,6 @@ export const getSupportedTokens = async () => {
 
   return SUPPORTED_TOKENS.map((s) => ({
     ...s,
-    price: prices[s.name],
+    price: prices[s.name]?.price,
   }));
 };
